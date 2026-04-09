b674aa6205ad045152b7d0e555772e7bfd54655c
MAHESH UMALE <mumale@gmail.com>
2026-04-09 07:34:02 +0530

    Complete implementation of OrderFlowEngine including analyze_candle, get_latest_analysis, and reset methods.

--- order_flow_engine.py
+++ order_flow_engine.py
@@ -1,6 +1,21 @@
 class OrderFlowEngine:
     def __init__(self):
-        pass
+        self.analysis = None
+        self.candles = []
 
-    def run(self):
-        pass
+    def analyze_candle(self, candle):
+        # Analyzing the candle data
+        # Example: simple logic to determine bullish/bearish
+        if candle['close'] > candle['open']:
+            self.analysis = 'bullish'
+        else:
+            self.analysis = 'bearish'
+        self.candles.append(candle)
+        return self.analysis
+
+    def get_latest_analysis(self):
+        return self.analysis
+
+    def reset(self):
+        self.analysis = None
+        self.candles.clear()
